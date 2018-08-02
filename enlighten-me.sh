7218c2bfda700943f26c607ceaba951332324a74
Ryan P. C. McQuen <ryanpcmcquen@member.fsf.org>
2018-08-02 06:02:35 -0700

    P. C.

--- enlighten-me.sh
+++ enlighten-me.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# Ryan P.C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
+# Ryan P. C. McQuen | Everett, WA | ryanpcmcquen@member.fsf.org
 
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
